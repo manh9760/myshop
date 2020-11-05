fc79063038d9de1c9ab0bf93ea721bb9cbeeada7
manh9760 <parabol123654@gmail.com>
2020-11-05 14:09:53 +0700

    Add facebook chat, remember change link by ngrok...

--- resources/views/layouts/guest_layout.blade.php
+++ resources/views/layouts/guest_layout.blade.php
@@ -95,6 +95,29 @@
 		}
 	</script>
 
+	<!-- Chat Facebook -->
+	<div id="fb-root"></div>
+	<script>
+		window.fbAsyncInit = function() {
+		  FB.init({
+		    xfbml: true,
+		    version: 'v8.0'
+		  });
+		};
+
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
+	<div class="fb-customerchat" attribution=setup_tool page_id="104284044828398" theme_color="#ff7e29"
+		logged_in_greeting="Chào mừng bạn đến với Cửa hàng Linh kiện máy tinh Văn Mạnh!!!"
+		logged_out_greeting="Chào mừng bạn đến với Cửa hàng Linh kiện máy tinh Văn Mạnh!!!">
+	</div>
+
 	<!-- Các file js thêm -->
 	@yield('script')
 </body>
